1d3ecbe7951b5f00523a80dc7605797c93b11af1
ashleynoriega <chapina.ashley@gmail.com>
2019-10-03 14:52:01 -0700

    adding ensg2hugo.py file

---
+++ ensg2hugo.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+import argparse
+import fileinput
+import re
+parser = argparse.ArgumentParser(description='Process CSV formate biodata.')
+parser.add_argument('bioname', metavar='FILENAME', type=str, nargs=1,
+                    help='an integer for the accumulator')
+parser.add_argument('-f', dest='column', type=int, default=1,
+                    help='sum the integers (default: find the max)')
+
+args = parser.parse_args()
+my_file= "Homo_sapiens.GRCh37.75.gtf"
+gene={}
+gene["gene_id"]="gene_name"
+for each_line_of_text in fileinput.input(my_file):
+    if re.match(r'.*\t.*\tgene\t', each_line_of_text):
+        gene_id_matches = re.findall('gene_id \"(.*?)\";', each_line_of_text)
+        gene_name_matches = re.findall('gene_name \"(.*?)\";', each_line_of_text)
+        if gene_id_matches and gene_name_matches:
+            gene[gene_id_matches[0]] = gene_name_matches[0]
+
+
+for each_line_of_text in fileinput.input(args.bioname):
+    columns = each_line_of_text.split(",")
+    if len(columns)>1:
+        match = re.search('(\w+)\.*(\w*)', columns[arg.column-1])
+        if match:
+            if match.group(1) in gene:
+                columns[arg.column -1]= '"' + gene[match.group(1)] + '"'
+            print ",".join(columns),
